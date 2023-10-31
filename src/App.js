abecb05cb23f9ffb03b8ea95c15a35a1f238f64e
Ali Akbar <chaudhary.ali.0737@gmail.com>
2023-10-31 19:54:11 +0500

    react redux learning started

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "./App.css";
-
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 import React from "react";
 import Todo from "./components/Todo/todo";
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
+const store = configureStore({
+  reducer: {
+
+  }
+})
 function App() {
   return (
     <>
-    {/* className={"button-65"} div={"div"} button={"button-64"} */}
-    <div className="app-div">
-    <Todo ></Todo>
-    </div>
+      {/* className={"button-65"} div={"div"} button={"button-64"} */}
+
+      <div className="app-div">
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App/>}>
+
+          </Route>
+        </Routes>
+        </BrowserRouter>
+        <Provider store={store}>
+          <Todo ></Todo>
+        </Provider>
+
+      </div>
     </>
   );
 }
